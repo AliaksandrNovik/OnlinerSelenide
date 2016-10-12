0640508e86d8435a8332f7302b73bb14cef944ff
Aliaksandr Novik <Aliaksandr_Novik2@epam.com>
2016-10-12 19:38:27 +0300

    implemented basic logic of task

--- src/test/java/by/rfe/novik/onliner/screens/catalog/BikePage.java
+++ src/test/java/by/rfe/novik/onliner/screens/catalog/BikePage.java
@@ -1,11 +1,13 @@
 package by.rfe.novik.onliner.screens.catalog;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 import java.util.List;
 
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.testng.TestException;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
@@ -15,11 +17,16 @@ public class BikePage {
     private static final String CSS_LOCATOR_LIST_BIKE_BRANDS =
             "div.schema-filter-popover.schema-filter-popover_visible span.schema-filter__checkbox-text";
 
+    private static final String CSS_LOCATOR_BIKES_PRESENT_MESSAGE = "div#schema-products";
+
     @FindBy(how = How.CSS, using = "div.schema-filter-control__item")
     private SelenideElement showAllBikesButton;
 
     @FindBy(how = How.CSS, using = "div.schema-product__title span")
     private List<SelenideElement> listOfBikesTitles;
+
+    @FindBy(how = How.CSS, using = "input[class*=number-input_price]")
+    private SelenideElement inputPriceFrom;
 
     public ElementsCollection getListBikeBrands() {
         showAllBikesButton.click();
@@ -28,6 +35,14 @@ public class BikePage {
 
     public List<SelenideElement> getListOfBikeTitles() {
         return listOfBikesTitles;
+    }
+
+    public void setPrice(final String priceForBikes) {
+        inputPriceFrom.setValue(priceForBikes);
+    }
+
+    public SelenideElement getBikesAreNotExistMessage() throws TestException {
+        return $(CSS_LOCATOR_BIKES_PRESENT_MESSAGE);
     }
 
 }
--- src/test/java/by/rfe/novik/onliner/tests/OnlinerTest.java
+++ src/test/java/by/rfe/novik/onliner/tests/OnlinerTest.java
@@ -1,9 +1,15 @@
 package by.rfe.novik.onliner.tests;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browser;
 import static com.codeborne.selenide.Selenide.open;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -12,14 +18,18 @@ import by.rfe.novik.onliner.screens.catalog.BeautyAndSportPage;
 import by.rfe.novik.onliner.screens.catalog.BikePage;
 import by.rfe.novik.onliner.screens.catalog.CatalogPage;
 
-@Test
+@Configuration
+@ComponentScan(basePackages = { "by.rfe.*" })
+@PropertySource(value = "classpath:api.properties")
 public class OnlinerTest {
+
+    private static final String priceForBikes = "100 000";
 
     BikePage bikePage;
 
     @BeforeClass(alwaysRun = true)
     public void beforeClass() {
-        // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\target\\classes\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
         browser = "chrome";
         baseUrl = "https://onliner.by";
         bikePage = navigateToBikePage();
@@ -37,7 +47,8 @@ public class OnlinerTest {
 
     @Test
     public void test03SetPriceAndCheckThatBikesAreNotExistedOnPage() {
-
+        bikePage.setPrice(priceForBikes);
+        bikePage.getBikesAreNotExistMessage().shouldHave(text("Упс! У нас нет таких товаров, попробуйте изменить условия поиска."));
     }
 
     private BikePage navigateToBikePage() {
@@ -47,4 +58,8 @@ public class OnlinerTest {
         return beautyAndSportPage.openBikePage();
     }
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
